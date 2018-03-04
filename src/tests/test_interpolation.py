e6f62c524be093f0047d68a980e820ceb68545cf
jeffwdoak <jeff.w.doak@gmail.com>
2018-03-04 16:05:38 +0000

    added first failing test

--- src/tests/test_interpolation.py
+++ src/tests/test_interpolation.py
@@ -3,6 +3,13 @@ Tests of functions to interpolate across geography.
 
 """
 
+import numpy as np
+
 import solar_energy
 
+def test_bilinear():
+    x = np.arange(10)
+    y = np.arange(10)
+    z = np.ones((10, 10))
+    f_interpolated = solar_energy.bilinear_interpolation(x, y, z)
 
