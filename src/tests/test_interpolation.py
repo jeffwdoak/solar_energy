780dd26f260930175e1588ffae7dc1fb6c22b520
jeffwdoak <jeff.w.doak@gmail.com>
2018-03-04 19:46:09 +0000

    added failing component to test_bilinear_interpolation

--- src/tests/test_interpolation.py
+++ src/tests/test_interpolation.py
@@ -4,6 +4,7 @@ Tests of functions to interpolate across geography.
 """
 
 import numpy as np
+from numpy.testing import assert_allclose
 
 import solar_energy
 
@@ -12,4 +13,4 @@ def test_bilinear():
     y = np.arange(10)
     z = np.ones((10, 10))
     f_interpolated = solar_energy.bilinear_interpolation(x, y, z)
-
+    assert_allclose(f_interpolated, np.ones((10, 10)))
